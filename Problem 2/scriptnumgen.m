03976cecb80e4965fb4d4aa1b23684a419408a2f
SiddarthMarwaha <siddharthmarwaha7192@gmail.com>
2015-12-08 10:14:25 -0500

    Solution to Problem 2
    This program generates a vector of random values between a small and
    large value provided by the user. The size of the vector is equal to the
    size provided by the user.

---
+++ Problem 2/scriptnumgen.m
@@ -0,0 +1,13 @@
+%This script calls the vecgen function
+%to generate a vector of random integers between the Min and Max values
+%provided by the user
+
+clc;
+
+Size = input('What size vector would you like? ');
+Size = round(Size);
+Min = input('Enter a small integer value: ');
+Max = input('Enter a large integer value: ');
+
+rndvec = vecgen(Size, Min, Max);
+
