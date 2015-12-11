4126a8754fcda6e14e0b96ac5168fbb4304633af
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-10 19:58:19 -0500

    Comments for Problem 4

--- Problem 4/script4matrixoperation.m
+++ Problem 4/script4matrixoperation.m
@@ -14,18 +14,33 @@ for i = 1:vecsize
     vec(i) = components;
 end
 
-pickoperation = menu('Pick an Operation', 'A*v' , '(v^T)*A' , '(v^T)*A*v');
-switch pickoperation
-    case 1
-        matrix = matrixoperation(vecsize, Min, Max, vec);
-        disp(matrix)
-    case 2
-        matrix = matrixoperation(vecsize, Min, Max, vec);
-        disp(matrix)
-    case 3
-        matrix = matrixoperation(vecsize, Min, Max, vec);
-        disp(matrix)
-    otherwise
-        disp('Thank you for your business')
-end
+% pickoperation = menu('Pick an Operation', 'A*v' , '(v^T)*A' , '(v^T)*A*v');
+% switch pickoperation
+%     case 1
+%         matrix = matrixoperation(vecsize, Min, Max, vec);
+%         disp(matrix)
+%     case 2
+%         matrix = matrixoperation(vecsize, Min, Max, vec);
+%         disp(matrix)
+%     case 3
+%         matrix = matrixoperation(vecsize, Min, Max, vec);
+%         disp(matrix)
+%     otherwise
+%         disp('Thank you for your business')
+% end
 
+choice = choosedialog;
+
+%I have added a "choosedialog" function that I basically copied over from: 
+%http://www.mathworks.com/help/matlab/ref/dialog.html. Since Matlab does not
+%recommend using menu, instead use dialog. I obviously changed its statement
+%and options, but it is essentially the same. Now to continue I just have 
+%one thing to say. I personally hate switch statements, that is why I did
+%not assign you to read over it lol. I suggest instead that you use if,
+%else if, and else statements to handle cases. Switch statements are good
+%if you know that there are only a finite number of cases, but the if-else
+%statements cover broader cases such as when we do not know all of the
+%cases.
+
+%Also practice adding the paths so that you do not have to keep copying
+%over files.
