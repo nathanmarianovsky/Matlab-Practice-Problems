be8d64b7d71590c7933f94dc87ed7eed06837c1c
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-11 15:22:47 -0500

    Problem 4 Comment/Functionality on Output Size

--- Problem 4/matrixoperation.m
+++ Problem 4/matrixoperation.m
@@ -1,15 +1,26 @@
-function matrix = matrixoperation(choice, Size, Min, Max, Vec)
+function matrix = matrixoperation(choice, inputsize, outputsize, Min, Max, Vec)
 
 %The function matrixopertaion is designed to perform three matrix/vector
 %operation: A*v, (v^T)*A, and (v^T)*A*v.  
 
-A = matrixgen3(Size, Min, Max);     %A is a matrix of random numbers between any min and max integers.
+% A = matrixgen(inputsize, Min, Max);     %A is a matrix of random numbers between any min and max integers.
+
+%Create a "m x n" matrix where m = outputsize and n = inputsize
+rndmatrixgen = zeros(outputsize, inputsize);
+
+%For each column
+for i = 1:inputsize
+    %Select the whole column and make it equate to a vector of length equal
+    %to the outputsize:
+    rndmatrixgen(:,i) = vecgen(outputsize, Min, Max);
+end
 
 if(strcmp(choice, 'A*v'))
-    matrix = A * Vec;
+    matrix = rndmatrixgen * Vec;
 elseif(strcmp(choice, '(v^T)*A'))
-    matrix = Vec.' * A;
+    matrix = Vec.' * rndmatrixgen;
 elseif(strcmp(choice, '(v^T)*A*v'))
-    matrix = Vec.' * A * Vec; 
+    matrix = Vec.' * rndmatrixgen * Vec; 
+end
 
 end
--- Problem 4/script.m
+++ Problem 4/script.m
@@ -1,41 +1,33 @@
+%This script calls the matrixoperation function to perform one of
+%the three operation available: A*v, (v^T)*A, and (v^T)*A*v.
 clc;
 
 addpath('../Problem 1/');
 addpath('../Problem 2/');
-addpath('../Problem 3/');
 
+%Min and Max are integers which are used to generate the matrix A, whrer A is a matrix
+%containing random integers between Min and Max
 Min = input('Enter a small integer value: ');
 Max = input('Enter a large integer value: ');
 
 fprintf('\n');
 
-vecsize = input('Enter the size of the vector: ');
-vecsize = round(vecsize);
-
-
-%Just thought I would tell you that you should always consider the user to
-%be an idiot. I am saying this because the way you handle inputs, the user
-%could go ahead, give a letter as input, and everything crashes. You should
-%make sure that the input you are getting is the one that you want.
-
-
-%Notice right below that I changed your vector from a row vector to a
-%column vector. I did this because your matrixoperation function expects a
-%column vector and here you are creating a row vector. Small technicality!
-
+vecsize = round(input('Enter the size (positive integer) of the vector which represents the input: '));
 
 vec = zeros(vecsize,1);
-% matrix = zeros(vecsize);
-
-%No need to preallocate the matrix!
 
 for i = 1:vecsize
     components = input('Enter the component of the vector: ');
     vec(i) = components;
 end
 
+%So here is the variable to contain our output size:
+output = round(input('Enter the size (positive integer) of the vector output size: '));
+
+%The function choosedialog creates a dropdown menu, provinding with three
+%different matrix operations and stores the choosen operation in choice
 choice = choosedialog;
-matrix = matrixoperation(choice, vecsize, Min, Max, vec);
+matrix = matrixoperation(choice, vecsize, output, Min, Max, vec);
 
 fprintf('The result of your choice of operation is:\n');
 disp(matrix);
